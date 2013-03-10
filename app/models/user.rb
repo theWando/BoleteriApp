53e9bf88c7235a1f28e56e01a62547c3d708864f
Eduardo Laguna <wrcdriver@gmail.com>
2013-03-10 10:03:36 -0430

    Funcionan las validaciones de la creación del usuario, si la contraseña es corta y/o distinta de la confirmación.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :birth_date, :email, :lastname, :name, :password, :type
+  attr_accessible :birth_date, :email, :lastname, :name, :password, :type, :password_confirmation
 
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i
 
-	belongs_to :passenger
-	belongs_to :employee
-
-  validates :name,			:presence => true
-  validates :lastname,	:presence => true
+  validates :name, :lastname, :type, :password_confirmation,			:presence => true
   validates :email, 		:presence => true, :uniqueness => true, :format => EMAIL_REGEX
-  validates :password,	:confirmation => true #password_confirmation attr
-  validates_length_of 	:password, :in => 6..20, :on => :create
-	validates	:type,			:presence => true
+  validates :password,	:length => { :minimum => 5, :maximum => 40 },
+												:confirmation => true #password_confirmation attr
 end
