d8343d8b64bbe878e1de8106d4cb69deff2b097a
Eduardo Laguna <wrcdriver@gmail.com>
2013-03-17 14:40:38 -0430

    Actualizado el gitignore para ignorar los archivos de reaspaldo creados por el gedit.
    Se envia información por defecto a la base de datos gracias al archivo seeds.rb
    
    Se actualiza el formulario para los usuarios usando internacionalización.
    
    Se modificó el application.html.erb para usar parciales en caso de que el usuario logeado sea un administrador o un empleado.
    
    Se crearon parciales (_employee_panel.html.erb y _admin_panel.html.erb) para ser mostrados en caso de que el usuario logeado sea parte del personal administrativo.
    
    Se corrige un type en el modelo travel (destinarion_country a destination_country)

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,7 +1,7 @@
 class Travel < ActiveRecord::Base
   attr_accessible :destination_city, :destination_country, :detail, :extract, :n_days, :n_nights
 
-	validates :destination_city, :destinarion_country, :detail, :extract, :n_days, :n_nights,	:presence => true
+	validates :destination_city, :destination_country, :detail, :extract, :n_days, :n_nights,	:presence => true
 	validates :destination_country,	:length	=> {:minimum => 4, :maximum => 59} #de acuerdo a los paises con los combres mas cortos y nombres mas largos
 	validates	:destination_city,		:length => {:minimum => 1, :maximum => 85}
 	
