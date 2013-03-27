175f6592881006e6e2bd2ae8725736f7feabf24c
Eduardo Laguna <wrcdriver@gmail.com>
2013-03-27 16:46:03 -0430

    Agrega la funcionalidad de registro y creacion de usuarios con devise

--- db/schema.rb
+++ db/schema.rb
@@ -51,20 +51,31 @@ ActiveRecord::Schema.define(:version => 20130322141001) do
     t.integer  "n_nights"
     t.text     "extract"
     t.text     "detail"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
-    t.integer  "number_of_seats_aviables
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+    t.integer  "number_of_seats_aviables"
   end
 
   create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.string   "name"
     t.string   "lastname"
-    t.string   "email"
-    t.string   "password"
-    t.date     "birth_date"
     t.string   "user_type"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "birth_date"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
